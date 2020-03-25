6f9603665b95444156958c3df6c1ba9e79e7d35e
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-25 07:47:14 +0100

    feat(client): Fetch Jobs in the Client

--- client/src/JobBoard.js
+++ client/src/JobBoard.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import { JobList } from './JobList';
-const { jobs } = require('./fake-data');
+import { loadJobs } from './requests';
 
 export class JobBoard extends Component {
-  render() {
-    return (
-      <div>
-        <h1 className="title">Job Board</h1>
-        <JobList jobs={jobs} />
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { jobs: [] };
+	}
+
+	async componentDidMount() {
+		const jobs = await loadJobs();
+		this.setState({ jobs });
+	}
+
+	render() {
+		const { jobs } = this.state;
+		return (
+			<div>
+				<h1 className="title">Job Board</h1>
+				<JobList jobs={jobs} />
+			</div>
+		);
+	}
 }
