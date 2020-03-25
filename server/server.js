474b9d11f69722d5ee933b0db3fca5bb97d57350
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-25 07:01:44 +0100

    feat(server): Configure Apollo Server with Express

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const { ApolloServer, gql } = require('apollo-server-express');
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
@@ -9,20 +12,29 @@ const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
 const app = express();
-app.use(cors(), bodyParser.json(), expressJwt({
-  secret: jwtSecret,
-  credentialsRequired: false
-}));
+app.use(
+	cors(),
+	bodyParser.json(),
+	expressJwt({
+		secret: jwtSecret,
+		credentialsRequired: false
+	})
+);
+
+const typeDefs = gql(fs.readFileSync('./schema.graphql', { encoding: 'utf8' }));
+const resolvers = require('./resolvers');
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+apolloServer.applyMiddleware({ app, path: '/graphql' });
 
 app.post('/login', (req, res) => {
-  const {email, password} = req.body;
-  const user = db.users.list().find((user) => user.email === email);
-  if (!(user && user.password === password)) {
-    res.sendStatus(401);
-    return;
-  }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
+	const { email, password } = req.body;
+	const user = db.users.list().find(user => user.email === email);
+	if (!(user && user.password === password)) {
+		res.sendStatus(401);
+		return;
+	}
+	const token = jwt.sign({ sub: user.id }, jwtSecret);
+	res.send({ token });
 });
 
 app.listen(port, () => console.info(`Server started on port ${port}`));
