c221a16eb6fed32016c12706b8c67711dfc2335a
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-25 08:07:06 +0100

    feat(client): Fetch a job from the client and display its data

--- client/src/JobDetail.js
+++ client/src/JobDetail.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { jobs } from './fake-data';
+import { loadJob } from './requests';
 
 export class JobDetail extends Component {
-  constructor(props) {
-    super(props);
-    const {jobId} = this.props.match.params;
-    this.state = {job: jobs.find((job) => job.id === jobId)};
-  }
+	constructor(props) {
+		super(props);
+		const { jobId } = this.props.match.params;
+		this.state = { job: null };
+	}
 
-  render() {
-    const {job} = this.state;
-    return (
-      <div>
-        <h1 className="title">{job.title}</h1>
-        <h2 className="subtitle">
-          <Link to={`/companies/${job.company.id}`}>{job.company.name}</Link>
-        </h2>
-        <div className="box">{job.description}</div>
-      </div>
-    );
-  }
+	async componentDidMount() {
+		const { jobId } = this.props.match.params;
+		const job = await loadJob(jobId);
+		this.setState({ job });
+	}
+
+	render() {
+		const { job } = this.state;
+		if (!job) {
+			return null;
+		}
+		return (
+			<div>
+				<h1 className="title">{job.title}</h1>
+				<h2 className="subtitle">
+					<Link to={`/companies/${job.company.id}`}>{job.company.name}</Link>
+				</h2>
+				<div className="box">{job.description}</div>
+			</div>
+		);
+	}
 }
